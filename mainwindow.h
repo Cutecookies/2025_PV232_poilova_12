527165d0536588f1559de6a3294253b0b72fa09e
cookie <118600927+Cutecookies@users.noreply.github.com>
2025-05-25 23:32:59 +0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPixmap>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void handleLoadButtonClick();
+    //void handleSaveButtonClick();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif
